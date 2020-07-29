39c60072a2ce33cfe16929ad8cfcef96090d72fd
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-07-29 13:53:36 +0100

    remove class play

--- actions.js
+++ actions.js
@@ -1,7 +1,20 @@
 // here are all the actions
-window.addEventListener('keydown',function(e){
-  // const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-  // if(!audio) return;
-  // audio.play();
-  console.log(e);
-});
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; // STOP THE FUNCTION FROM RUNNING IF THERE ISN'T AN AUDIO ELEMENT FOR THE PRESSED KEY
+    audio.play();
+    audio.currentTime = 0; //is going to rewind to the start
+    key.classList.add("playing");
+}
+window.addEventListener('keydown',playSound);
+
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+  
+}
